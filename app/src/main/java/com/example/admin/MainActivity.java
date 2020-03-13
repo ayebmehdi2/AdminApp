f8122298360e4dbde66ec58c313917216b9b7fee
Mehdi Ayeb <mehdiayeb14@gmail.com>
2020-03-13 21:42:57 +0100

    final

--- app/src/main/java/com/example/admin/MainActivity.java
+++ app/src/main/java/com/example/admin/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.admin;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,7 +17,11 @@ import androidx.databinding.DataBindingUtil;
 import com.example.admin.databinding.ActivityMainBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -38,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
     private int counter = 0;
     private ActivityMainBinding binding;
     private ArrayList<String> cod = new ArrayList<>();
+    private String NA = "Non connue !";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +53,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        String uid = preferences.getString("uid", null);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference();
+
+        reference.child("ADMINS").child(uid).child("name").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.getValue(String.class);
+                if (name != null) NA = name;
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) { }
+        });
 
 
         binding.genrate.setOnClickListener(new View.OnClickListener() {
@@ -57,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
 
                 @SuppressLint("SimpleDateFormat") DateFormat simple = new SimpleDateFormat("dd MMM yyyy HH:mm");
                 Date result = new Date(System.currentTimeMillis());
-                name = simple.format(result);
+                name = simple.format(result) + NA;
                 if (!(name.length() > 0)){
                     Toast.makeText(MainActivity.this, "You must choose name", Toast.LENGTH_LONG).show();
                     return;
@@ -70,23 +93,28 @@ public class MainActivity extends AppCompatActivity {
                     return;
                 }
 
-                for (int i = 0; i < Integer.valueOf(num); i++){
+                for (int i = 0; i < Integer.parseInt(num); i++){
                     String chifre =  String.valueOf(ThreadLocalRandom.current().nextInt(100000000, 999999999));
                     switch (prod){
                         case 1:
                             chifre = "produit1--" + chifre;
+                            name = "produit1--" + name;
                             break;
                         case 2:
                             chifre = "produit2--" + chifre;
+                            name = "produit2--" + name;
                             break;
                         case 3:
                             chifre = "produit3--" + chifre;
+                            name = "produit3--" + name;
                             break;
                         case 4:
                             chifre = "produit4--" + chifre;
+                            name = "produit4--" + name;
                             break;
                         case 5:
                             chifre = "produit5--" + chifre;
+                            name = "produit5--" + name;
                             break;
                         default:
                             chifre = "0";
@@ -96,7 +124,6 @@ public class MainActivity extends AppCompatActivity {
                 }
                 counter = 0;
                 new AsyncT().execute(cod.get(counter));
-
 
 
             }
